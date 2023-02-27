ad43e96a43347eb7ff2e0bb5dd758344dbc6e0da
Annely Link <anne.for.git@gmail.com>
2023-02-27 23:16:29 +0300

    ДЗ.1

--- 1.1-first-project/first_project/app/views.py
+++ 1.1-first-project/first_project/app/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render, reverse
+import datetime
+import os
 
 
 def home_view(request):
@@ -8,8 +10,8 @@ def home_view(request):
     # функцию `reverse`
     pages = {
         'Главная страница': reverse('home'),
-        'Показать текущее время': '',
-        'Показать содержимое рабочей директории': ''
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
     }
     
     # context и параметры render менять не нужно
@@ -23,7 +25,7 @@ def home_view(request):
 def time_view(request):
     # обратите внимание – здесь HTML шаблона нет, 
     # возвращается просто текст
-    current_time = None
+    current_time = datetime.datetime.now().time()
     msg = f'Текущее время: {current_time}'
     return HttpResponse(msg)
 
@@ -32,4 +34,11 @@ def workdir_view(request):
     # по аналогии с `time_view`, напишите код,
     # который возвращает список файлов в рабочей 
     # директории
-    raise NotImplemented
+    base_path = os.getcwd()
+    print(base_path)
+    x = os.listdir(base_path)
+    msg = f'Cписок файлов: {x}'
+    # raise NotImplemented
+    return HttpResponse(msg) # => .ideaappdb.sqlite3first_projectmanage.pyREADME.mdrequirements.txtvenv
+                           # .idea app db.sqlite3 first_project manage.py README.md requirements.txt venv
+
