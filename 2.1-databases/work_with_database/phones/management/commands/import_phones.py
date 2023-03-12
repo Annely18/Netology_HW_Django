5b50290fe1a81c8cef5414a9bb5c1d2782e17351
Annely Link <anne.for.git@gmail.com>
2023-03-13 01:20:45 +0300

    ДЗ_2.1_phones

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -25,24 +25,3 @@ class Command(BaseCommand):
                 slug=slugify(phone['name'])
                 )
             ph.save()
-
-# import csv
-#
-# from django.core.management.base import BaseCommand
-# from phones.models import Phone
-#
-#
-# class Command(BaseCommand):
-#     def add_arguments(self, parser):
-#         pass
-#
-#     def handle(self, *args, **options):
-#         with open('phones.csv', 'r') as file:
-#             phones = list(csv.DictReader(file, delimiter=';'))
-#
-#         for phone in phones:
-#             Phone.objects.create(name=phone['name'], price=phone['price'], image=phone['image'],
-#                                  release_date=phone['release_date'], lte_exists=phone['lte_exists'])
-
-
-
